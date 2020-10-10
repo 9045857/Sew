ba93e032d2ae50f8308d1935c22bf192728175f6
9045857 <9045857@mail.ru>
2020-10-10 20:14:31 +0700

    after

--- ForSew/Deal.cs
+++ ForSew/Deal.cs
@@ -7,7 +7,7 @@ namespace ForSew
         public DateTime DealMoment { get; set; }
         public DealTypes DealType { get; set; }
         public float DealAmount { get; set; }
-               
+               //test after 
         public Deal(DateTime dealMoment, DealTypes dealType, float dealAmount)
         {
             DealMoment = dealMoment;
