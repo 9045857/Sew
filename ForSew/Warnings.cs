32181a00321de6d6eed196537227ac0b209dae7a
9045857 <9045857@mail.ru>
2020-10-14 22:46:38 +0700

    сделал класс событий для лога

---
+++ ForSew/Warnings.cs
@@ -0,0 +1,15 @@
+﻿namespace ForSew
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Warnings
+    {
+        public const string FileNotExist = "Файл {0} не существует";
+        public const string DealDontParsed = "Ошибка парсинга Сделки. Файл: '{0}', строка: '{1}'";
+        public const string DealDateTimeDontParsed = "Ошибка парсинга ДатыВремяСделки. Исходный текст: {0}";
+        public const string DealTypeDontParsed = "Ошибка парсинга ТипаСделки. Исходный текст: {0}";
+        public const string DealAmontDontParsed = "Ошибка парсинга СуммаСделки. Исходный текст: {0}";
+
+    }
+}
